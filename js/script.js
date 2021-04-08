aaae5fa09f013d436436038c62ff8724f63f3222
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-04-08 17:24:12 +0200

    Aggiunto l'esercizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-// Inserisci il codice qui
+// https://flynn.boolean.careers/exercises/api/array/music
+// colore body #1E2D3B
+// colore header/card #2E3A46
+// font Lato
+
+var app = new Vue (
+  {
+    el: "#app",
+    data: {
+      oggetti: []
+    },
+    mounted: function() {
+      {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((risposta) => {
+
+          var oggetto = risposta.data.response;
+          for (var item in oggetto) {
+
+            this.oggetti.push(oggetto[item]);
+
+          }
+
+          console.log(this.oggetti);
+
+        })
+      }
+    }
+  }
+);
